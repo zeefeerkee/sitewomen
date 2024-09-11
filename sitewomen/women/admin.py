2cff1ba9bc27ad1eaa4671897e6147f99195fa58
zeefeerkee <finekii@vk.com>
2024-09-11 20:29:21 +0500

    add: admin-panel

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,3 +1,74 @@
-from django.contrib import admin
+from typing import Any
+from django.contrib import admin, messages
+from django.db.models.functions import Length
+from django.db.models.query import QuerySet
+from .models import Women, Category
 
-# Register your models here.
+
+class MarriedFilter(admin.SimpleListFilter):
+    title = "Статус женщин"
+    parameter_name = "status"
+
+    def lookups(self, request: Any, model_admin: Any) -> list[tuple[Any, str]]:
+        return [
+            ("married", "Замужем"),
+            ("single", "Не замужем"),
+        ]
+
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value() == "married":
+            return queryset.filter(husband__isnull=False)
+        if self.value() == "signle":
+            return queryset.filter(husband__isnull=True)
+
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    # fields = ("title", "content", "slug", "cat",)
+    # exclude = ("tags", "is_published", )
+    # readonly_fields = ("slug", )
+    prepopulated_fields = {"slug": ("title",)}
+    filter_horizontal = ("tags",)
+    list_display = ("title", "time_create", "is_published", "cat", "brief_info")
+    list_display_links = ("title",)
+    ordering = ("-time_create", "title")
+    list_editable = ("is_published",)
+    list_per_page = 5
+    actions = (
+        "set_published",
+        "set_draft",
+    )
+    search_fields = (
+        "title",
+        "cat__name",
+    )
+    list_filter = (
+        MarriedFilter,
+        "cat__name",
+        "is_published",
+    )
+
+    @admin.display(description="Краткое описание", ordering=Length("content"))
+    def brief_info(self, women: Women):
+        return f"Описание {len(women.content)} символов"
+
+    @admin.action(description="Опубликовать выбранные записи")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Опубликовано {count} записей")
+
+    @admin.action(description="Снять с публикации выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(
+            request, f"Снято с публикации {count} записей", messages.WARNING
+        )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+
+
+# admin.site.register(Women)
--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,5 +1,7 @@
+from tabnanny import verbose
 from django.db import models
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 
 
 # Create your models here.
@@ -13,26 +15,44 @@ class Women(models.Model):
         DRAFT = 0, "Черновик"
         PUBLISHED = 1, "Опубликовано"
 
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
     slug = models.SlugField(
-        max_length=255,
-        unique=True,
-        db_index=True,
+        max_length=255, unique=True, db_index=True, verbose_name="Слаг"
     )
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    content = models.TextField(blank=True, verbose_name="Содержание")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
+    is_published = models.BooleanField(
+        choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+        default=Status.DRAFT,
+        verbose_name="Статус",
+    )
     cat = models.ForeignKey(
-        "Category", on_delete=models.PROTECT, related_name="articles"
+        "Category",
+        on_delete=models.PROTECT,
+        related_name="articles",
+        verbose_name="Категория",
     )
-    tags = models.ManyToManyField("TagArticle", blank=True, related_name="tags")
+    tags = models.ManyToManyField(
+        "TagArticle", blank=True, related_name="tags", verbose_name="Тэги"
+    )
     husband = models.OneToOneField(
-        "Husband", on_delete=models.SET_NULL, null=True, blank=True, related_name="wife"
+        "Husband",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="wife",
+        verbose_name="Муж",
     )
 
     objects = models.Manager()
     published = PublishManager()
+
+    class Meta:
+        verbose_name = "Известные женщины"
+        verbose_name_plural = "Известные женщины"
+        ordering = ["-time_create"]
+        indexes = [models.Index(fields=["-time_create"])]
 
     def __str__(self):
         return self.title
@@ -40,14 +60,16 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse("article", kwargs={"article_slug": self.slug})
 
-    class Meta:
-        ordering = ["-time_create"]
-        indexes = [models.Index(fields=["-time_create"])]
-
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+    slug = models.SlugField(
+        max_length=255, unique=True, db_index=True, verbose_name="Слаг"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def get_absolute_url(self):
         return reverse("category", kwargs={"cat_slug": self.slug})
